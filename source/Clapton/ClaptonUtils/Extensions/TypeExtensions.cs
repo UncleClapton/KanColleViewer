04964f6c646e8376acbdcc49ea0ef86bb9781444
UncleClapton <camjwelt@gmail.com>
2015-11-18 10:36:09 +0900

    Add Clapton Utility Library to the project

---
+++ source/Clapton/ClaptonUtils/Extensions/TypeExtensions.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace Clapton.Extensions
+{
+    public static class TypeExtensions
+    {
+        /// <summary>
+        /// Determines wrather the specified property name is a valid property within a type.
+        /// </summary>
+        /// <param name="type"></param>
+        /// <param name="propertyName"></param>
+        /// <returns></returns>
+        public static bool HasProperty(this Type type, string propertyName)
+        {
+            return type.GetProperty(propertyName) != null;
+        }
+
+        /// <summary>
+        /// Finds all Types derived from the current <see cref="Type"/> within a given <see cref="Assembly"/>
+        /// </summary>
+        /// <param name="baseType"></param>
+        /// <param name="assembly">The <see cref="Assembly"/> in which to search for derived types.</param>
+        /// <returns><see cref="IEnumerable{Type}"/> containing all found derived types</returns>
+        public static IEnumerable<Type> FindDerivedTypes(this Type baseType, Assembly assembly)
+        {
+            return assembly.GetTypes().Where(t => t != baseType && baseType.IsAssignableFrom(t));
+        }
+    }
+}
---
+++ source/Clapton/ClaptonUtils/Xml/IO.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Xml.Serialization;
+
+namespace Clapton.Xml
+{
+    public static class IO
+    {
+		public static T TryReadXml<T>(this string filePath) where T : new()
+		{
+			if (filePath == null || !File.Exists(filePath))
+			{
+				throw new FileNotFoundException("File not found.", filePath);
+			}
+
+			FileStream stream = null;
+			T result = new T();
+            var serializer = new XmlSerializer(typeof(T));
+
+            try
+			{
+				stream = new FileStream(filePath, FileMode.Open);
+                result = (T)serializer.Deserialize(stream);
+			}
+            catch (Exception)
+            {
+                //Add error handling to fit your purposes.
+            }
+			finally
+			{
+				if (stream != null)
+				{
+					stream.Close();
+					stream.Dispose();
+				}
+			}
+
+			return result;
+		}
+
+        public static bool TryWriteXml<T>(this T saveData, string savePath) where T : new()
+        {
+            var dir = Path.GetDirectoryName(Path.GetFullPath(savePath)) ?? "";
+            Directory.CreateDirectory(dir);
+
+            FileStream stream = null;
+            var serializer = new XmlSerializer(typeof(T));
+
+            try
+            {
+                stream = new FileStream(savePath, FileMode.Create);
+                serializer.Serialize(stream, saveData);
+            }
+            catch (Exception)
+            {
+                //Add error handling to fit your purposes.
+                return false;
+            }
+            finally
+            {
+                if (stream != null)
+                {
+                    stream.Close();
+                    stream.Dispose();
+                }
+            }
+            return true;
+        }
+    }
+}
